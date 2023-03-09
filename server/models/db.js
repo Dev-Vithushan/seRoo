b89155311d5aee7dd46bf67aa4043ed2a147364e
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2023-03-09 17:18:40 +0530

    small changes added

--- server/models/db.js
+++ server/models/db.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 }).then(() => {
   console.log('Connected to MongoDB and Node');
 }).catch((error) => {
